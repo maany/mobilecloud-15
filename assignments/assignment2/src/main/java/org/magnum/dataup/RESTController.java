b340d5205fffdf0034715e7d26e24b4e76791a83
Mayank Sharma <imptodefeat@gmail.com>
2015-07-09 02:03:21 +0530

    add documentation/ refine for submission

--- assignments/assignment2/src/main/java/org/magnum/dataup/RESTController.java
+++ assignments/assignment2/src/main/java/org/magnum/dataup/RESTController.java
@@ -3,9 +3,8 @@ package org.magnum.dataup;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
@@ -23,13 +22,29 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+/**
+ * This class is a Spring Controller that handles requests to the following endpoints :
+ * <ul>
+ * <li>GET /video
+ * <li>POST /video
+ * <li>GET /video/{id}/data
+ * <li>POST /video/{id}/data
+ * </ul>
+ * 
+ * @author Mayank
+ */
 @Controller
 public class RESTController {
+	/**
+	 * {@link java.util.List} stores the {@link Video} objects.
+	 */
 	List<Video> videos = new ArrayList<Video>();
 	
+//==========================
+//	Begin Handler Mappings
+//==========================
 	@RequestMapping(value="/video",method=RequestMethod.GET)
 	public @ResponseBody List<Video> fetchVideoMetaData(){
-		System.out.println("Inside addVideoMetadata() Handler method");
 		return videos;
 	}
 	
@@ -37,12 +52,11 @@ public class RESTController {
 	public @ResponseBody Video addVideoMetaData(@RequestBody Video video,HttpServletRequest request){
 		// Generate unique ID
 		video.setId(generateRandomId(video));
-		// generate data url
+		// Generate dataUrl
 		video.setDataUrl(generateDataUrl(video,request));
 		videos.add(video);
 		return video;
 	}
-
 
 	@RequestMapping(value="/video/{id}/data", method=RequestMethod.POST)
 	public @ResponseBody VideoStatus uploadVideo(@PathVariable("id") String id,@RequestParam("data") MultipartFile file, HttpServletResponse response){
@@ -61,15 +75,13 @@ public class RESTController {
 			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
 			return null;
 		}
-		
-		System.out.println("inside upload");
-		System.out.println(response.toString());
+
 		return new VideoStatus(VideoState.READY);
 	}
 	
 	@RequestMapping(value="/video/{id}/data", method=RequestMethod.GET)
 	public void sendVideoToClient(@PathVariable String id,HttpServletResponse response){
-		System.out.println("received download request");
+		response.setContentType("multipart/mp4");
 		ServletOutputStream out=null;
 		Video video=null;
 		video = getVideoById(id);
@@ -80,13 +92,17 @@ public class RESTController {
 			VideoFileManager.get().copyVideoData(video, out);
 			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
 			e.printStackTrace();
 		}catch(Exception ex){
 			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
 		}
 	}
+//==========================
+//	End Handler Mappings
+//==========================
+	
+	
 	/**
 	 * Randomly generate a {@link Long} value for id.
 	 * @param video
